46650409a1f14a3538ccd91e6f8854d18fe6b67a
GuyTevet <guy.tvt@gmail.com>
2019-08-07 19:03:43 +0300

    run file

---
+++ MASS/run_summarization.sh
@@ -0,0 +1,21 @@
+MODEL=mass_en_1024.pth 
+
+python train.py                                      \
+--exp_name mass_summarization                        \
+--data_path ./data/processed/summarization/          \
+--lgs 'ar-ti'                                        \
+--mt_steps 'ar-ti'                                   \
+--encoder_only false                                 \
+--emb_dim 1024                                       \
+--n_layers 6                                         \
+--n_heads 8                                          \
+--dropout 0.2                                        \
+--attention_dropout 0.2                              \
+--gelu_activation true                               \
+--tokens_per_batch 3000                              \
+--optimizer adam_inverse_sqrt,beta1=0.9,beta2=0.98,lr=0.0001 \
+--epoch_size 200000                                  \
+--max_epoch 20                                       \
+--eval_bleu true                                     \
+--english_only true                                  \
+--reload_model "$MODEL,$MODEL"
