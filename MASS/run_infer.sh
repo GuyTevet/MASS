ba03d63b1c9a3e04a325af3347bc47bcfc4812ad
Guy Tevet <guy.tvt@gmail.com>
2019-08-09 22:12:51 +0300

    inferece script

---
+++ MASS/run_infer.sh
@@ -0,0 +1,1 @@
+python translate_ensemble.py --exp_name giga_test --src_lang ar --tgt_lang ti --beam 5 --batch_size 1 --model_path ./dumped/copy_mass_summarization/qtfpweuu4d/checkpoint.pth --output_path ./infer/output.txt.beam5 < ./data/processed/giga/test.ar-ti.ar
