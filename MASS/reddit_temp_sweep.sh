895202b0ab73b5f01fe6ffae5e83e5c5948782f2
Guy Tevet <guy.tvt@gmail.com>
2020-03-07 16:04:32 +0200

    script update

--- MASS/reddit_temp_sweep.sh
+++ MASS/reddit_temp_sweep.sh
@@ -3,7 +3,7 @@ SAMPLES=10
 #TEMPS=(
 #    0.1 0.2 0.5 0.8 1 1.1 1.2 1.4 1.6
 #    )
-TEMPS=($(seq 0.2 0.01 1.21))
+TEMPS=($(seq 0.05 0.01 1.16))
 
 for t in "${TEMPS[@]}"; do
         bash infer_reddit_dialog.sh -s $SAMPLES -t $t
